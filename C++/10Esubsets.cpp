bf8a60ff96270900a1a600d5b20675d0bd99b0ed
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-07-06 22:06:02 +0530

    one problem

---
+++ C++/10Esubsets.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int arr[50];
+    int subs[20][20];
+    int size;
+    cin >> size;
+    int j=0, k=0;
+    for (int i=0;i<size;i++){
+        cin >> arr[i];      
+    }
+    for (int i = 0; i < k;i++)
+    {
+
+    }
+
+        return 0;
+}
